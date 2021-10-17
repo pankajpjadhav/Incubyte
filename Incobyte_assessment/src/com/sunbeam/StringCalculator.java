3c4856757ce2c47d58d30949627120fefb8f8bc0
Pankaj Jadhav <pankajjadhav1740@gmail.com>
2021-10-18 00:05:09 +0530

    test-ninth completed

--- Incobyte_assessment/src/com/sunbeam/StringCalculator.java
+++ Incobyte_assessment/src/com/sunbeam/StringCalculator.java
@@ -2,9 +2,9 @@ package com.sunbeam;
 
 import java.util.ArrayList;
 
-
 public class StringCalculator {
-	private static int count=0;
+	private static int count = 0;
+
 	public static int getCount() {
 		return count;
 	}
@@ -19,8 +19,8 @@ public class StringCalculator {
 
 			if (nums.charAt(i) >= 48 && nums.charAt(i) <= 57) {
 				str += nums.charAt(i);
-				if(str.length()==1 && i>0 && nums.charAt(i-1)==45)
-                    throw new NegativeValueException(("Negative Values Not Allowed"));
+				if (str.length() == 1 && i > 0 && nums.charAt(i - 1) == 45)
+					throw new NegativeValueException(("Negative Values Not Allowed"));
 			}
 
 			else {
@@ -34,20 +34,23 @@ public class StringCalculator {
 		if (str.length() > 0)
 			list.add(str);
 		int sum = 0;
-		for (int i = 0; i < list.size(); i++)
-			sum += Integer.parseInt(list.get(i));
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i).equals("1000") || list.get(i).length() < 4) {
+				sum += Integer.parseInt(list.get(i));
+			}
+		}
 		return sum;
 
 	}
 
 	public static void main(String[] args) {
-		
+
 		try {
-			System.out.println(add("1,2 ,3"));
+			System.out.println(add("1,2 ,1001"));
 			System.out.println(add("1,4 , 1,4 ,5"));
 			System.out.println(add("7,8,10"));
-			//System.out.println(add("2/***;-3"));
-			//System.out.println(add("-2/***;-3"));
+			// System.out.println(add("2/***;-3"));
+			// System.out.println(add("-2/***;-3"));
 			System.out.println(getCount());
 		} catch (InvalidInputException | NegativeValueException e) {
 			System.out.println(e.getMessage());
