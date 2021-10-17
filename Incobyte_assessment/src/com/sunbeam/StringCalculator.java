a28ee43f2f5008656823075ffa29f654bba0299b
Pankaj Jadhav <pankajjadhav1740@gmail.com>
2021-10-17 23:56:05 +0530

    test-fifth completed

--- Incobyte_assessment/src/com/sunbeam/StringCalculator.java
+++ Incobyte_assessment/src/com/sunbeam/StringCalculator.java
@@ -5,15 +5,18 @@ import java.util.ArrayList;
 
 public class StringCalculator {
 
-	public static int add(String nums) throws InvalidInputException {
+	public static int add(String nums) throws InvalidInputException, NegativeValueException {
 		if (nums.length() == 0)
 			return 0;
 		ArrayList<String> list = new ArrayList<>();
 		String str = "";
 		for (int i = 0; i < nums.length(); i++) {
 
-			if (nums.charAt(i) >= 48 && nums.charAt(i) <= 57)
+			if (nums.charAt(i) >= 48 && nums.charAt(i) <= 57) {
 				str += nums.charAt(i);
+				if(str.length()==1 && i>0 && nums.charAt(i-1)==45)
+                    throw new NegativeValueException(("Negative Values Not Allowed"));
+			}
 
 			else {
 				if (str.length() > 0)
@@ -38,8 +41,8 @@ public class StringCalculator {
 			System.out.println(add("1,2 ,3"));
 			System.out.println(add("1,4 , 1,4 ,5"));
 			System.out.println(add("7,8,10"));
-			System.out.println(add("2/***;3"));
-		} catch (InvalidInputException e) {
+			System.out.println(add("2/***;-3"));
+		} catch (InvalidInputException | NegativeValueException e) {
 			System.out.println(e.getMessage());;
 		}
 	}
